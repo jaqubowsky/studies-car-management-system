a62c84387d50510bfb434bc4caeb81b0be061bda
Jakub Nalewajk <jaqubowskyy@gmail.com>
2024-11-02 23:12:17 +0100

    Refactor main.py to use AppService for car management system

--- main.py
+++ main.py
@@ -1,41 +1,10 @@
 from services.car_service import CarService
 from services.menu_service import Menu
-
-CHOICES = {
-    'NEW_CAR': '1',
-    'REMOVE_CAR': '2',
-    'LIST_CARS': '3',
-    'EXIT': '4'
-}
+from services.app_service import AppService
 
 car_service = CarService()
 menu_service = Menu()
-
-def run_system():
-    while True:
-        menu_service.init_main_menu()
-        choice = input("Choose an option (1-4): ")
-
-        if choice == CHOICES['NEW_CAR']:
-            make, model, year, price = menu_service.get_add_car_inputs()
-            car_service.add_car(make, model, year, price)
-
-        elif choice == CHOICES['REMOVE_CAR']:
-            car_service.list_cars()
-
-            id = menu_service.get_remove_car_inputs()
-
-            car_service.remove_car(id)
-
-        elif choice == CHOICES['LIST_CARS']:
-            car_service.list_cars()
-
-        elif choice == CHOICES['EXIT']:
-            print("Exiting system. Goodbye!")
-            break
-
-        else:
-            print("Invalid choice. Please select a valid option.")
+app_service = AppService(car_service, menu_service)
 
 if __name__ == "__main__":
-    run_system()
+    app_service.run_system()
---
+++ services/app_service.py
@@ -0,0 +1,46 @@
+CHOICES = {
+    'NEW_CAR': '1',
+    'REMOVE_CAR': '2',
+    'LIST_CARS': '3',
+    'EXIT': '4'
+}
+
+class App:
+    def __init__(self, car_service, menu_service):
+        self.car_service = car_service
+        self.menu_service = menu_service
+
+    def add_new_car(self):
+        inputs = self.menu_service.get_add_car_inputs()
+        if inputs is None:
+            return
+        make, model, year, price = inputs
+        self.car_service.add_car(make, model, year, price)
+
+    def remove_car(self):
+        self.car_service.list_cars()
+        id = self.menu_service.get_remove_car_inputs()
+        if id is None:
+            return
+        self.car_service.remove_car(id)
+
+    def run_system(self):
+        while True:
+            self.menu_service.init_main_menu()
+            choice = self.menu_service.get_user_choice()
+
+            if choice == CHOICES['NEW_CAR']:
+                self.add_new_car()
+
+            elif choice == CHOICES['REMOVE_CAR']:
+                self.remove_car()
+
+            elif choice == CHOICES['LIST_CARS']:
+                self.car_service.list_cars()
+
+            elif choice == CHOICES['EXIT']:
+                self.menu_service.exit_system()
+                break
+
+            else:
+                self.menu_service.invalid_choice()
