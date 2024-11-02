fc1bfec0e550ef1aedcff289bf3244cdd55761da
Jakub Nalewajk <jaqubowskyy@gmail.com>
2024-11-02 22:51:19 +0100

    Refactor Car class constructor to include an ID parameter and rename the price parameter

--- models/car.py
+++ models/car.py
@@ -1,9 +1,11 @@
+
 class Car:
-    def __init__(self, make, model, year, price=0):
+    def __init__(self, make: str, model: str, year: int, _price: float = 0, _id: int = 0):
+        self._id = _id
         self.make = make
         self.model = model
         self.year = year
-        self.price = price
+        self._price = _price
 
-    def __str__(self):
-        return f"{self.year} {self.make} {self.model} - ${self.price:,.2f}"
+    def __str__(self) -> str:
+        return f"{self.year} {self.make} {self.model} - ${self._price:,.2f}"
